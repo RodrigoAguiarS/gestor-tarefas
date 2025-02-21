16075da8d413af7917bdf1e27abe072d19685ee5
Rodrigo Aguiar <86214343+RodrigoAguiarS@users.noreply.github.com>
2025-02-21 10:30:10 -0300

    adiciona a entindade tarefas, e os enum de prioridade e situacao.

---
+++ src/main/java/br/com/rodrigo/gestortarefas/api/model/response/UsuarioComTarefasConcluidasResponse.java
@@ -0,0 +1,17 @@
+package br.com.rodrigo.gestortarefas.api.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioComTarefasConcluidasResponse {
+
+    private UsuarioResponse usuario;
+    private Long quantidadeTarefasConcluidas;
+
+}
