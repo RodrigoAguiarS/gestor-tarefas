5138aadde911aa4503447bee7a8857e5b645122d
Rodrigo Aguiar <86214343+RodrigoAguiarS@users.noreply.github.com>
2025-05-02 02:43:07 -0300

    test: adiciona mock de serviço de notificações e verifica chamadas no teste de notificação por perfil

--- src/test/java/br/com/rodrigo/gestortarefas/api/teste/SseServiceTest.java
+++ src/test/java/br/com/rodrigo/gestortarefas/api/teste/SseServiceTest.java
@@ -2,6 +2,7 @@ package br.com.rodrigo.gestortarefas.api.teste;
 
 import br.com.rodrigo.gestortarefas.api.model.Usuario;
 import br.com.rodrigo.gestortarefas.api.repository.UsuarioRepository;
+import br.com.rodrigo.gestortarefas.api.services.INotificacao;
 import br.com.rodrigo.gestortarefas.api.services.SseService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class SseServiceTest {
@@ -25,6 +27,9 @@ class SseServiceTest {
 
     @InjectMocks
     private SseService sseService;
+
+    @Mock
+    private INotificacao notificacaoService;
 
     private static final Long USUARIO_ID = 1L;
 
@@ -74,19 +79,24 @@ class SseServiceTest {
 
     @Test
     void notificarPorPerfil_DeveNotificarTodosUsuariosComPerfil() {
+        // Arrange
         long perfilId = 1L;
         Usuario usuario1 = new Usuario();
         usuario1.setId(1L);
         Usuario usuario2 = new Usuario();
         usuario2.setId(2L);
         List<Usuario> usuarios = Arrays.asList(usuario1, usuario2);
+        String mensagem = "Test message";
 
         when(usuarioRepository.findAllByPerfisId(perfilId)).thenReturn(usuarios);
 
-        String mensagem = "Test message";
+        // Act
         sseService.notificarPorPerfil(mensagem, perfilId);
 
+        // Assert
         verify(usuarioRepository).findAllByPerfisId(perfilId);
+        verify(notificacaoService).criarNotificacao(usuario1, mensagem);
+        verify(notificacaoService).criarNotificacao(usuario2, mensagem);
     }
 
     @Test
